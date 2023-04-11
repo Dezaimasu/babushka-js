2e5d6b557834039128c949cb3372966b2b4a62f3
Dezaimasu <niderlandz@mail.ru>
2023-04-11 18:59:15 +0300

    move cards between stock and waste

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const suits = {
 const ranks = [...Array(9).keys()].map(r => (r + 2).toString()).concat(['J', 'Q', 'K', 'A']);
 
 function buildDeck(){
-  const deck = [];
+  let deck = [];
 
   function shuffle(array){
     for (let i = array.length - 1, downTo = 1; i >= downTo; i--) {
@@ -20,19 +20,23 @@ function buildDeck(){
 
   Object.entries(suits).forEach(([suit, icon]) => {
     ranks.forEach(rank => {
-      const newCard = {suit, icon, rank, slotName: null, slotIndex: null};
+      const newCard = {suit, icon, rank, slotName: null, index: null};
       deck.push(newCard);
     });
   });
 
-  return shuffle(deck);
+  deck = shuffle(deck);
+
+  return deck.map((card, index) => {
+    card.index = index;
+    return card;
+  });
 }
 
 const deck = buildDeck();
 
 function drawCard(card){
   const cardNode = document.querySelector('#templates > .card').cloneNode(true);
-  cardNode.dataset.suit = card.suit;
 
   cardNode.querySelector('.card-top').innerHTML = `<div>${card.rank}<br>${card.icon}</div><div class="debug"></div><div>${card.rank}<br>${card.icon}</div>`;
   cardNode.querySelector('.card-mid').innerHTML = `<div>${card.icon}</div>`;
@@ -43,14 +47,30 @@ function drawCard(card){
 
 function placeCard(card, slotName, upturned){
   const cardNode = drawCard(card);
-  // upturned = true; // TODO: remove after debug
+  upturned = true; // TODO: remove after debug
   cardNode.dataset.upturned = upturned ? '1': '0';
-  cardNode.dataset.slot = slotName; // TODO: remove after debug
+  cardNode.dataset.suit = card.suit;
+  cardNode.dataset.index = card.index;
+  cardNode.dataset.debug = `${card.index}-${slotName}`; // TODO: remove after debug
 
   const slot = slots[slotName];
   slot.node.insertBefore(cardNode, null);
   slot.cards.push(card);
   card.slotName = slotName;
+}
+
+function removeCard(card){
+  document.querySelector(`.card[data-index='${card.index}']`).remove();
+}
+
+function moveCard(card, from, to, upturned = true){
+  const removedCard = slots[from].cards.pop();
+  if (removedCard.suit !== card.suit || removedCard.rank !== card.rank) {
+  	alert('ERROR');
+  }
+
+  removeCard(card);
+  placeCard(card, to, upturned);
 }
 
 const slots = {
@@ -69,22 +89,37 @@ const slots = {
   pile6       : {cards: [], node: document.querySelector('#pile6')},
 };
 
-let cardIndex = 0;
-for (let pileIndex = 0, pilesTotal = 7; pileIndex < pilesTotal; pileIndex++) {
-  for (let indexInPile = 0, cardsInPileTotal = pileIndex; indexInPile <= cardsInPileTotal; indexInPile++) {
-    placeCard(
-      deck[cardIndex++],
-      `pile${pileIndex}`,
-      indexInPile === cardsInPileTotal
-    );
+function moveCardsBetweenStockAndWaste(){
+  const stockCards = slots.stock.cards;
+
+  if (stockCards.length === 0) {
+    const wasteCardIndexes = slots.waste.cards.map(card => card.index);
+    wasteCardIndexes.reverse().forEach(cardIndex => {
+      moveCard(deck[cardIndex], 'waste', 'stock', false);
+    });
+  } else {
+    moveCard(stockCards.at(-1), 'stock', 'waste');
   }
 }
 
-const cardsInPiles = 28;
-deck.slice(cardsInPiles).forEach(card => {
-  placeCard(card, 'stock', false);
-});
+function init(){
+  let cardIndex = 0;
+  for (let pileIndex = 0, pilesTotal = 7; pileIndex < pilesTotal; pileIndex++) {
+    for (let indexInPile = 0, cardsInPileTotal = pileIndex; indexInPile <= cardsInPileTotal; indexInPile++) {
+      placeCard(
+        deck[cardIndex++],
+        `pile${pileIndex}`,
+        indexInPile === cardsInPileTotal
+      );
+    }
+  }
 
-deck.forEach(card => {
-  console.log(card.slotName);
-});
+  const cardsInPiles = 28;
+  deck.slice(cardsInPiles).forEach(card => {
+    placeCard(card, 'stock', false);
+  });
+
+  slots.stock.node.addEventListener('click', moveCardsBetweenStockAndWaste);
+}
+
+init();
